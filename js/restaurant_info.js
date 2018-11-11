b78876856298be608a47b12fde450c65788ea609
Laura Degeratu <loveable_koneko@yahoo.com>
2018-11-11 12:39:14 +0100

    add map token

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoia29uZWtvdGUiLCJhIjoiY2pvY3M4d2Q4MTBraDNxcTk1cHc3MXY3aSJ9.XsLuzD59H1B5MXWo729TfA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
